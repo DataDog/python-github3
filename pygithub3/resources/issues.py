4d0493a3e208ed265410a7bb4879bf2251e36361
Isaac Sadaqah <isaac@datadoghq.com>
2014-05-09 11:32:56 -0400

    Add accessor to get an issue's pull request

--- pygithub3/resources/issues.py
+++ pygithub3/resources/issues.py
@@ -46,6 +46,8 @@ class Issue(Resource):
     def __str__(self):
         return '<Issue (%s)>' % getattr(self, 'number', '')
 
+    def pull_request(self):
+        return self._maps.get('pull_request')	
 
 class Comment(Resource):
 
